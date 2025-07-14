ac736c60a603b3844985be8e468be267c75e8a72
Abner Cerqueira <abner.cerqueira@fatec.sp.gov.br>
2025-07-14 20:34:59 -0300

    feat: adiciona corrente para erros http

---
+++ src/main/http/errors/impl/400/invalid-params-validator.ts
@@ -0,0 +1,17 @@
+import { InvalidParamsError } from '../../../../../domains/shared/errors/invalid-params-error.ts'
+import type { HandlerParams } from '../../chain-of-errors-handler.ts'
+import { ChainOfErrorsHandlerImpl } from '../chain-of-error-handler-impl.ts'
+
+export class InvalidParamsValidator extends ChainOfErrorsHandlerImpl {
+  handle(params: HandlerParams): void {
+    const { reply, error } = params
+
+    if (error instanceof InvalidParamsError) {
+      const { message } = error
+      reply.status(400).send({ message })
+      return
+    }
+
+    super.handle(params)
+  }
+}
